67960442764e201d291012a47ce7473767c9b5e7
Wisn <wuyishun_kmk@outlook.com>
2017-09-07 19:18:59 +0800

    update build.gradle

---
+++ gradlew.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# ./gradlew clean :app:assembleRelease
+# ./gradlew clean :skin1:assembleRelease
+# ./gradlew clean :skin2:assembleRelease
+ ./gradlew  app:assembleRelease
+ ./gradlew  skin1:assembleRelease
+ ./gradlew  skin2:assembleRelease
