fb89ed2ea7a9399ab3b3bb56d722245dd7668e85
Wisn <wuyishun_kmk@outlook.com>
2017-09-19 11:08:32 +0800

    fix initSkinLoader

--- skinlib/src/main/java/com/wisn/skinlib/base/SkinApplication.java
+++ skinlib/src/main/java/com/wisn/skinlib/base/SkinApplication.java
@@ -1,7 +1,8 @@
 package com.wisn.skinlib.base;
 
 import android.app.Application;
-import android.os.Environment;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 
 import com.wisn.skinlib.SkinManager;
@@ -29,14 +30,27 @@ public class SkinApplication extends Application {
         /*SkinManager.getInstance().setSkinRootPath(Environment.getExternalStorageDirectory() +
                                                   File.separator +
                                                   "dd");*/
-        SkinManager.getInstance().setSkinRootPath( SkinConfig.SP_Default_Skin_Root_Path);
-        initSkinLoader();
-        //夜间模式使用
-        if (SpUtils.isNightMode(this)) {
-            SkinManager.getInstance().nightMode();
-        } else {
-            SkinManager.getInstance().loadSkin(null);
-        }
+        SkinManager.getInstance().setSkinRootPath(SkinConfig.SP_Default_Skin_Root_Path);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                initSkinLoader();
+               new Handler(Looper.getMainLooper()).post(
+                       new Runnable() {
+                           @Override
+                           public void run() {
+                               //夜间模式使用
+                               if (SpUtils.isNightMode(SkinApplication.this)) {
+                                   SkinManager.getInstance().nightMode();
+                               } else {
+                                   SkinManager.getInstance().loadSkin(null);
+                               }
+                           }
+                       }
+               );
+            }
+        }).start();
+
     }
 
     private void initSkinLoader() {
@@ -45,9 +59,10 @@ public class SkinApplication extends Application {
             //拷贝assets到皮肤目录
             if (skinFile == null || skinFile.length == 0) return;
             for (String fileName : skinFile) {
-                Log.d("SkinApplication","   "+fileName);
-                File toFile = new File(SkinFileUitls.getSkinPath(this,false), fileName);
-                Log.d("SkinApplication","   "+SkinFileUitls.getSkinPath(this,false)+File.separator+fileName);
+                Log.d("SkinApplication", "   " + fileName);
+                File toFile = new File(SkinFileUitls.getSkinPath(this, false), fileName);
+                Log.d("SkinApplication",
+                      "   " + SkinFileUitls.getSkinPath(this, false) + File.separator + fileName);
                 if (!toFile.exists()) {
                     toFile.createNewFile();
 //                    toFile.mkdirs();
@@ -55,17 +70,17 @@ public class SkinApplication extends Application {
                 }
             }
             //检测皮肤目录和解压目录的一致性
-            String skinPath = SkinFileUitls.getSkinPath(this,false);
-            File file=new File(skinPath);
-            if(!file.exists())return ;
+            String skinPath = SkinFileUitls.getSkinPath(this, false);
+            File file = new File(skinPath);
+            if (!file.exists()) return;
             String[] Skin = file.list();
-            if(Skin==null)return ;
+            if (Skin == null) return;
             for (String fileName : Skin) {
-                File skinRes = new File(SkinFileUitls.getSkinPath(this,true), fileName);
+                File skinRes = new File(SkinFileUitls.getSkinPath(this, true), fileName);
                 if (skinRes.exists() && skinRes.isDirectory()) {
                     continue;
                 }
-                SkinFileUitls.upZipSkin(this, skinPath + File.separator + fileName,fileName);
+                SkinFileUitls.upZipSkin(this, skinPath + File.separator + fileName, fileName);
             }
         } catch (IOException e) {
             e.printStackTrace();
