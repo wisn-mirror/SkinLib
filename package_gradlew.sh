c1ddefe419cb9e70174b4f567bd0fc175d26375a
Wisn <wuyishun_kmk@outlook.com>
2017-12-25 15:49:16 +0800

    update android studio 3.0.1 gradle 4.1

--- package_gradlew.sh
+++ package_gradlew.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-# ./gradlew clean :app:assembleRelease
- ./gradlew clean :skin1:assembleRelease
- ./gradlew clean :skin2:assembleRelease
- ./gradlew clean :skin3:assembleRelease
+ ./gradlew clean :app:assembleRelease
+#  ./gradlew clean :skin1:assembleRelease
+# ./gradlew clean :skin2:assembleRelease
+#  ./gradlew clean :skin3:assembleRelease
 # ./gradlew  app:assembleRelease
 # ./gradlew  skin1:assembleRelease
 # ./gradlew  skin2:assembleRelease
